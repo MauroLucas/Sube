c4bd6514e8899a02122aa5ab48ef1c5c14a172e7
MauroLucas <m_l_pereyra@hotmail.com>
2018-05-23 12:20:11 -0300

    Agrego test de traer seccion por recorrido

--- Proyecto/Sube/src/test/TestTraerSeccionesPorRecorrido.java
+++ Proyecto/Sube/src/test/TestTraerSeccionesPorRecorrido.java
@@ -8,8 +8,8 @@ public class TestTraerSeccionesPorRecorrido {
 	public static void main(String[] args) {
 		SeccionRecorridoDao sr = new SeccionRecorridoDao();
 		//System.out.println(Funciones.imprimirLista(sr.traer()));
-		Parada origen = ParadaABM.getInstance().traer((long)15);
-		Parada destino = ParadaABM.getInstance().traer((long)8);
+		Parada origen = ParadaABM.getInstance().traer((long)1);
+		Parada destino = ParadaABM.getInstance().traer((long)9);
 		//System.out.println(origen);
 		//System.out.println(destino);
 		System.out.println(sr.traer(origen, destino));
